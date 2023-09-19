d58ee898a6dd03ac63150fa789a13aff3a7f18f4
Chandru-Ghub <chandruinfo455@gmail.com>
2023-09-19 19:48:50 +0530

    added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors(
     
     {
 
-    origin:'https://gentle-biscuit-57ba17.netlify.app',
+    origin:'http://localhost:5173',
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }
